b5a57aaded8b042f6a0d1e585594319d174d5a4f
mateoclarke <code@mateoclarke.com>
2015-04-04 18:21:42 -0500

    Add del module. Add watch task and refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,75 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var mainBowerFiles = require('main-bower-files');
 var inject = require('gulp-inject');
+var del = require('del');
 
 var paths = {
-	temp : 'temp',
-	tempVendor : 'temp/vendor',
-	index : 'app/index.html'
+	temp: 'temp',
+	tempVendor: 'temp/vendor',
+	tempIndex: 'temp/index.html',
+
+	index: 'app/index.html',
+	appSrc: ['app/**/*.js', '!app/index.html'],
+	bowerSrc: 'bower_components/**/*'
 }
 
-gulp.task('default', ['scripts', 'serve']);
+gulp.task('default', ['watch']);
 
-gulp.task('scripts', function(){
-	var tempIndex = gulp.src(paths.index).pipe(gulp.dest(paths.temp));
+gulp.task('watch', ['serve'], function(){
+	gulp.watch(paths.appSrc, ['scripts']);
+	gulp.watch(paths.bowerSrc + '/**/*', ['vendors']);
+	gulp.watch(paths.index, ['copyAll']);
 
-	// This copies all js files inside our app folder to the temp folder
-	var scripts = gulp.src('app/**/*.js').pipe(gulp.dest(paths.temp));
+})
 
-	var tempVendors = gulp.src(mainBowerFiles()).pipe(gulp.dest(paths.tempVendor));
-
-
-	tempIndex.pipe(inject(scripts, {
-		relative: true
-	})).pipe(inject(tempVendors, {
-		relative: true, 
-		name: 'vendorInject'
-	})).pipe(gulp.dest(paths.temp));
+gulp.task('serve', ['copyAll'], function(){
+	return gulp.src(paths.temp)
+		.pipe(webserver({
+			livereload: true,
+			proxies: [{
+				source: '/api',
+				target: 'http:localhost:1337'
+		}]
+	}));
 });
 
+gulp.task('copyAll', function(){
+	var tempVendors = gulp.src(mainBowerFiles()).pipe(gulp.dest(paths.tempVendor));
+	var appFiles = gulp.src(paths.appSrc).pipe(gulp.dest(paths.temp));
+	
+	return gulp.src(paths.index)
+		.pipe(gulp.dest(paths.temp))
+		.pipe(inject(tempVendors, {
+			relative: true,
+			name: 'vendorInject'
+		}))
+		.pipe(inject(appFiles, {
+			relative: true
+		}))
+		.pipe(gulp.dest(paths.temp));
+});
 
-// This launches our webserver from the 'temp' directory
-gulp.task('serve', function(){
-	gulp.src(paths.temp)
-	.pipe(webserver({
-		open: true					// opens browser
-		// directoryListing: true	// shows directory listing on main page
-	}));
+gulp.task('vendors', function(){
+	var tempVendors = gulp.src(mainBowerFiles()).pipe(gulp.dest(paths.tempVendor));
+
+	return gulp.src(paths.tempIndex)
+		.pipe(inject(tempVendors, {
+			relative: true, 
+			name: 'vendorInject'
+		}))
+		.pipe(gulp.dest(paths.temp));
+});
+
+gulp.task('scripts', function(){
+	var appFiles = gulp.src(paths.appSrc).pipe(gulp.dest(paths.temp));
+
+	return gulp.src(paths.tempIndex)
+		.pipe(inject(appFiles, {
+			relative: true
+		}))
+		.pipe(gulp.dest(paths.temp));
+});
+
+gulp.task('clean', function(){
+	del([paths.temp]);
 });
