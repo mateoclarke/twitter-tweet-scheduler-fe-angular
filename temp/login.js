a75b009ca84d3973fcaad3973f797730b73e082d
mateoclarke <code@mateoclarke.com>
2015-04-08 20:27:26 -0500

    Add Logout button and function

--- temp/login.js
+++ temp/login.js
@@ -1,9 +1,14 @@
 angular.module('app').controller('Login', function($scope, $auth, $http){
+	
+	$scope.isAuthenticated = $auth.isAuthenticated;
+	
 	$scope.login = function() {
 		$auth.authenticate('twitter');
 	}
 
-	$scope.isAuthenticated = $auth.isAuthenticated;
+	$scope.logout = function(){
+		$auth.logout();
+	}
 
 	$scope.tweet = function(){
 		$http.post('/api/post/tweet', '').then(function(){
