f7ae7592046c22d200cba1d21cc07234a6a46c4c
mateoclarke <code@mateoclarke.com>
2015-04-13 13:42:25 -0500

    Add n  service to fetch id of a post

--- app/post.js
+++ app/post.js
@@ -1,39 +1,45 @@
-angular.module('app').controller('Post', function($scope, $http, $location){
+	angular.module('app').controller('Post', function($scope, $http, $location){
 
-	var id = $location.search().id;
+		var id = $location.search().id;
 
-	console.log(id);
+		if(id) getPost();
 
-	$scope.tweet = function(){
+		$scope.tweet = function(){
 
-		var datetime = new Date(
-			$scope.date.getFullYear()
-			, $scope.date.getMonth()
-			, $scope.date.getDate()
-			, $scope.time.getHours()
-			, $scope.time.getMinutes()
-		)
+			var datetime = new Date(
+				$scope.date.getFullYear()
+				, $scope.date.getMonth()
+				, $scope.date.getDate()
+				, $scope.time.getHours()
+				, $scope.time.getMinutes()
+			)
 
-		$http.post('/api/post/tweet', { 
-			message: $scope.message,
-			datetime: datetime
-		}).then(function(){
+			$http.post('/api/post/tweet', { 
+				message: $scope.message,
+				datetime: datetime
+			}).then(function(){
 
-		});
-	}
+			});
+		}
 
-	$scope.time = new Date();
-	
-	$scope.minDate = new Date();
+		$scope.time = new Date();
+		
+		$scope.minDate = new Date();
 
-	$scope.opened = false;
+		$scope.opened = false;
 
-	$scope.open = function($event){
-		$event.preventDefault();
-		$event.stopPropagation();
+		$scope.open = function($event){
+			$event.preventDefault();
+			$event.stopPropagation();
 
-		$scope.opened = !$scope.opened;
-	}
+			$scope.opened = !$scope.opened;
+		}
+
+		function getPost(){
+			$http.get('/api/post/' + id).then(function(post){
+				$scope.post = post;
+			})
+		}
 
 
-})
+	})
