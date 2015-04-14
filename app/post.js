461fd81f22eb9bbc13310e4e7398564492aad4e8
mateoclarke <code@mateoclarke.com>
2015-04-13 19:06:48 -0500

    Try to address bug between Angular 1.3 and Angular-UI

--- app/post.js
+++ app/post.js
@@ -71,5 +71,15 @@
 			return id;
 		}
 
+	})
 
+	angular.module('app').directive('datepickerPopup', function(){
+		return {
+			restrict: 'EAC',
+			require: 'ngModel',
+			link: function(scope, element, attr, controller) {
+				// remove the default formatter from the input directive to prevent conflict
+				controller.$formatters.shift();
+			}
+		}
 	})
